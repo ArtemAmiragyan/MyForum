6d7a247d76b0effbd14ffe9bde2f431d8f06829d
artemAmiragyan <artyom.amiragyan@yandex.ru>
2018-09-15 23:43:56 +0300

    added html

---
+++ mainPage.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Блог</title>
+	<link rel="stylesheet" type="text/css" href="mainCss.css">
+</head>
+<body>
+ 
+	<div class="wrapper">
+		<div class="header">
+            <div id="logo">
+                <p><img  width="250" height="200" src="images/artem.jpg"></p>  
+            </div>    
+            </p>Блог Артема Амирагяна</p>
+        </div>
+		<div class="left-sidebar">Статьи</div>
+		<div class="content">
+        <form action="addInDb.php" method="POST">
+            <p><b>Есть что рассказать?</b></p>
+            <p><textarea cols="60" rows="5" placeholder="Я сходил покушать"></textarea></p>
+            <p><a class="bot8"  href="#">Поделиться</a></p>
+        </form>
+		</div>
+			<div class="clear"></div>
+		<div class="footer">Quiwi 89043440881</div>
+	</div>
+ 
+</body>
+</html>
